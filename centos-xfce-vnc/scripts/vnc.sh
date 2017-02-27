678223d9cf969465bf10a3219b4ace9a4fbfdf7b
iMeiji <361183736@qq.com>
2017-02-27 18:21:38 +0800

    test

--- centos-xfce-vnc/scripts/vnc.sh
+++ centos-xfce-vnc/scripts/vnc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -rf /root/.vnc/*.log
-killall firefox-esr > /dev/null 2>&1
+killall firefox > /dev/null 2>&1
 export DISPLAY=localhost:0.0
-/usr/lib/firefox-esr/firefox-esr
+/usr/lib/firefox/firefox

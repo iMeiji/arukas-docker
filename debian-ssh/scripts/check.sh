9ed19001de6253acc831aa562f747f1bcdee84a4
iMeiji <361183736@qq.com>
2017-02-27 13:00:02 +0800

    update scripts

--- debian-ssh/scripts/check.sh
+++ debian-ssh/scripts/check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if ps -ef|grep "firefox-esr"|grep -v "grep"
-then
+if ps -ef|grep "/firefox-esr"|grep -v "grep"
+	then
 	echo "ff is running!" `date` >> /var/log/check.log
 else
 	echo "ff is topped!"  `date` >> /var/log/check.log
